adc793db5fdfb98c077074d3b47ab8393f189b64
Nexusrex <ahmedtahoun55@gmail.com>
2025-02-11 22:33:09 +0200

    Initial commit

---
+++ FormattingVisitor.py
@@ -0,0 +1,103 @@
+from JavaParser import JavaParser
+from JavaParserVisitor import JavaParserVisitor
+from antlr4.TokenStreamRewriter import TokenStreamRewriter
+
+class FormattingVisitor(JavaParserVisitor):
+    def __init__(self, tokens, config):
+        self.rewriter : TokenStreamRewriter = TokenStreamRewriter(tokens)
+        self.config = config
+        self.indent_level: int = 0
+    
+    def visitClassDeclaration(self, ctx: JavaParser.ClassDeclarationContext):
+        class_name = ctx.identifier().getText()
+        modifiers = []
+        parent = ctx.parentCtx
+
+        if isinstance(parent, JavaParser.TypeDeclarationContext):
+            if parent.classOrInterfaceModifier():
+                modifiers = [mod.getText() for mod in parent.classOrInterfaceModifier()]
+                modifiers = self._sort_modifiers(modifiers, self.config.get("class_modifier_order", []))
+        
+        class_signature = f"{' '.join(modifiers)} class {class_name}".strip()
+        self.rewriter.replaceRangeTokens(parent.start, ctx.identifier().stop, class_signature)
+
+        class_body = ctx.classBody()
+
+        if class_body:
+            open_brace = class_body.LBRACE().symbol
+            close_brace = class_body.RBRACE().symbol
+
+            self._remove_whitespace(open_brace.tokenIndex + 1)
+            
+            self._remove_whitespace(close_brace.tokenIndex - 1)
+
+            if self.config.get("brace_style", "attach") == "attach":
+                self.rewriter.replaceSingleToken(open_brace, " {")
+            else:
+                self.rewriter.replaceSingleToken(open_brace, "\n" + self._get_indent() + "{")
+
+            self.rewriter.replaceSingleToken(close_brace, "\n" + self._get_indent() + "}")
+
+            self.indent_level += 1
+
+        
+        return self.visitChildren(ctx)
+    
+    def visitMethodDeclaration(self, ctx: JavaParser.MethodDeclarationContext):
+        return_type = ctx.typeTypeOrVoid().getText()
+        method_name = ctx.identifier().getText()
+        modifiers = []
+        parent = ctx.parentCtx  
+        if isinstance(parent, JavaParser.MemberDeclarationContext):  
+            grandparent = parent.parentCtx  
+            if isinstance(grandparent, JavaParser.ClassBodyDeclarationContext):
+                if grandparent.modifier():
+                    modifiers = [mod.getText() for mod in grandparent.modifier()]
+                    modifiers = self._sort_modifiers(modifiers, self.config.get("method_modifier_order", []))
+
+        method_signature = f"{' '.join(modifiers)} {return_type} {method_name}".strip()
+
+        self.rewriter.replaceRangeTokens(grandparent.start, ctx.identifier().stop, "\n" + self._get_indent() + method_signature)
+
+        method_body = ctx.methodBody()
+        if method_body and method_body.block():
+            block = method_body.block()
+            open_brace = block.LBRACE().getSymbol()
+            close_brace = block.RBRACE().getSymbol()
+
+            self._remove_whitespace(open_brace.tokenIndex - 1)
+
+            if self.config.get("brace_style", "attach") == "attach":
+                self.rewriter.replaceSingleToken(open_brace, " {")
+            else:
+                self.rewriter.replaceSingleToken(open_brace, "\n" + self._get_indent() + "{")
+
+            self.rewriter.replaceSingleToken(close_brace, "\n" + self._get_indent() + "}")
+
+        return self.visitChildren(ctx)
+    
+    # NEEDS A LOT OF WORK
+    def visitBlock(self, ctx: JavaParser.BlockContext):
+        self.indent_level += 1
+        for statement in ctx.blockStatement():
+            statement_start = statement.start
+            
+            self.rewriter.insertBeforeToken(statement_start, "\n" + self._get_indent())
+
+        self.indent_level -= 1
+        return self.visitChildren(ctx)
+    
+    def _remove_whitespace(self, pos):
+        token = self.rewriter.getTokenStream().get(pos)
+        if token.text.isspace():
+            self.rewriter.deleteToken(token)
+
+    def _sort_modifiers(self, modifiers, order):
+        return sorted(modifiers, key=lambda x: order.index(x) if x in order else len(order))
+    
+    def _get_indent(self):
+        return " " * (self.indent_level * self.config.get("indent_size", 4))
+    
+    def get_formatted_code(self, tree):
+        self.visit(tree)
+        return self.rewriter.getDefaultText()
--- test.java
+++ test.java
@@ -1,4 +1,15 @@
 public class HelloWorld{
-public static void main(String[]args){System.out.println("Hello, World!");}
+    public static void main(String[] args) {System.out.println("Hello World");int a = 10;}
+    public void test(){System.out.println("Hello World");}
+    public void test2()
+    {
+        if (true)
+        {
+            System.out.println("Hello World");
+        }
+        else
+        {
+            System.out.println("Hello World");
+        }
+    }
 }
-
