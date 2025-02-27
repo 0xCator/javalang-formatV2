53ff224145ca845b36c43741fee3f80085742cec
Nexusrex <ahmedtahoun55@gmail.com>
2025-02-27 23:35:07 +0200

    Reimplemented the space alignment to fix bugs and handle tabs.

--- FormattingVisitor.py
+++ FormattingVisitor.py
@@ -279,11 +279,11 @@ class FormattingVisitor(JavaParserVisitor):
                 max_parameter_size = self.config.aligns['parameters_before_align']
                 if not parameter_size > max_parameter_size:
                     return
-
+                
                 for i in range(max_parameter_size, parameter_size, max_parameter_size):
                     capture_pos = i * 2
                     parameter = parameters.getChild(capture_pos)
-                    align_spaces = " " * (open_paren.column + 1)
+                    align_spaces = self._get_align_spaces(open_paren)
                     self.rewriter.insertBeforeToken(parameter.start, f"\n{align_spaces}")
 
             case 'dont_align':
@@ -313,8 +313,18 @@ class FormattingVisitor(JavaParserVisitor):
                 for i in range(1, parameter_size):
                     capture_pos = i * 2
                     parameter = parameters.getChild(capture_pos)
-                    align_spaces = " " * (open_paren.column + 1)
+                    align_spaces = self._get_align_spaces(open_paren)
                     self.rewriter.insertBeforeToken(parameter.start, f"\n{align_spaces}")
+
+    def _get_align_spaces(self, open_paren):
+        token_stream = self.rewriter.getTokenStream()
+        token_index = open_paren.tokenIndex
+        spacer_length = 0
+        while token_stream.get(token_index).line == open_paren.line:
+            spacer_length += len(token_stream.get(token_index).text)
+            token_index -= 1
+
+        return " " * (spacer_length + (self.indent_level * self.config.indents['size']))         
 
     def _remove_whitespace(self, pos):
         while self.rewriter.getTokenStream().get(pos).type in [JavaParser.WS] or self.rewriter.getTokenStream().get(pos).text == "\n":
